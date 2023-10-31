e40c8e7db7b9e99de9bc23a66799ec754c872f2e
Joseph <jok7.marrone.123@gmail.com>
2023-10-31 07:28:34 +0100

    Create Thread.cpp

---
+++ src/internal/Thread.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <thread>
+
+class Thread {
+private:
+    std::function<void()> func;
+    std::thread tObj;
+
+public:
+    Thread(std::function<void()> tFunc) : func(tFunc) {}
+
+    void start() {
+        tObj = std::thread(func);
+    }
+
+    void join() {
+        if(tObj.joinable()) {
+            tObj.join();
+        }
+    }
+};
