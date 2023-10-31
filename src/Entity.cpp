3fd4f722ee4ea940746fc2ee69e86b791ac0a1da
Joseph <jok7.marrone.123@gmail.com>
2023-10-31 11:21:58 +0100

    Create Entity.cpp

---
+++ src/Entity.cpp
@@ -0,0 +1,17 @@
+class Entity {
+private:
+    float x, y; // Posizione dell'entità
+
+    int hp; // Quantità di HP 
+    int exp; // Esperienza
+public:
+    Entity(float startX, float startY) : x(startX), y(startY) {}
+
+    float getX() const { return x; }
+    float getY() const { return y; }
+
+    void updatePosition(float deltaX, float deltaY) {
+        x += deltaX;
+        y += deltaY;
+    }
+};
